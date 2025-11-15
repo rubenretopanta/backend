50a3db9a4afe8235e5bd510f58e0f314c8f90286
unknown <rubenretopanta@gmail.com>
2025-11-14 20:28:41 -0500

    actualizando v13

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(_cors);
 
 
 app.get("/", (req, res) => {
-res.send("Servidor funcionando con GitHub Actions v12");
+res.send("Servidor funcionando con GitHub Actionsgit v13");
 });
 
 app.use("/api/v1", api);
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,10 @@
 import _mongoose from 'mongoose';
 
-// Conexión básica
+
 _mongoose.connect('mongodb://localhost:27017/bdprueba')
 .then(() => console.log('Conectado a MongoDB'))
 .catch(err => console.error('Error de conexión:', err));
 
-// Manejar eventos de conexión
 _mongoose.connection.on('connected', () => {
   console.log('Mongoose conectado');
 });
@@ -18,7 +17,7 @@ _mongoose.connection.on('disconnected', () => {
   console.log('Mongoose desconectado');
 });
 
-// Cerrar conexión al terminar la aplicación
+
 process.on('SIGINT', async () => {
   await _mongoose.connection.close();
   process.exit(0);
